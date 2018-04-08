4634879ac20ef63f5b5ab43f2ca1505a7ec9e810
Shurumov <alexei0113@gmail.com>
2018-04-08 22:12:18 +0700

    remove axios and jquery, change work with sessionFromNative, change display subtitle

--- js/scripts.js
+++ js/scripts.js
@@ -1,58 +1,31 @@
-
-
-var termsListArray, favoriteArray, session, project;
+var termsListArray, favoriteArray, session, project, baseUrl;
 startLoadingAnimation();
 
-//Получаем json
-//axios.post('https://api.sbercode.appercode.com/v1/sbercode_ca/login', {
-//        "username": "abbreviations",
-//        "password": "gth7596"
-//    })
-//    .then(function (response) {
-//        session = response.data.sessionId;
-//        getList();
-//
-//    })
-//    .catch(function (error) {
-//        console.log(error);
-//    });
+
 
 function sessionFromNative(e){
 	var userData = JSON.parse(e);
     session = userData.sessionId;
     project = userData.projectName;
-    getList(session, project)
+    baseUrl = userData.baseUrl;
+    getList(session, project, baseUrl)
 }
 
-function getList(session, project) {
-    axios({
-            method: 'get',
-            url: 'https://api.sbercode.appercode.com/v1/'+ project +'/objects/Abbreviations?take=800&order=title',
-            headers: {
-                'X-Appercode-Session-Token': session
-            }
-        })
-        .then(function (response) {
-            termsListArray = response.data;
-            
-            createList();
-            var screenHeight = screen.height,
-                alphabetHeight = document.getElementsByClassName("list-terms__letter-list")[0].offsetHeight;
+function getList(session, project, base) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', base + project + '/objects/Abbreviations?take=800&order=title', false);
+    xhr.setRequestHeader('X-Appercode-Session-Token', session);
+    xhr.send();
 
-            if ((screenHeight*0.8) < alphabetHeight) {
-               document.getElementsByClassName("list-terms__letter-list")[0].remove();
-            }
-            
+    if (xhr.status != 200) {
 
-        })
-        .catch(function (error) {
-
-            console.log(error);
-        })
-}
-
-
-
+        alert(xhr.status + ': ' + xhr.statusText); 
+    } else {
+        termsListArray = JSON.parse(xhr.responseText);
+        
+        createList();
+    }
+};
 
 
 
@@ -61,16 +34,12 @@ function getList(session, project) {
 var listTermsElements = document.getElementsByClassName('list-terms__elements')[0];
 
 function createList() {
-    
-    var letterWidth = getTextWidth("a", "normal 13px sans-serif");
-    var lineWidth = screen.width;
-    
-    var lettersQuantity = Math.round(lineWidth / letterWidth);
-
     var groups = document.getElementsByClassName('list-terms__group');
     var groupTitle;
+    console.log(termsListArray);
+    console.log(termsListArray.length);
+    
     termsListArray.forEach(function (item, i, termsListArray) {
-
 
         //создание группы и хедера 
 
@@ -113,59 +82,19 @@ function createList() {
 
             var itemSubtitle = document.createElement('div');
             itemSubtitle.className = "list-terms__item-subtitle";
-            itemSubtitle.innerHTML = stringTruncation(termsListArray[i].html, lettersQuantity);
+            itemSubtitle.innerHTML = termsListArray[i].html;
             item.appendChild(itemSubtitle);
 
         }
     });
-    
     stopLoadingAnimation();
 }
 
-
-
 var listTerms = document.getElementsByClassName('list-terms')[0];
-
-
-
-
-
-
-
-//пролистывание списка при наведении на буквы
-
-var toHeader = debounce(goToHeader, 100);
-
-function goToHeader(event) {
-    var target = event.target;
-    var targetIndex;
-
-    var Headers = document.getElementsByClassName("list-terms__header-group");
-    var groupHeaders = [];
-    for (var i = 0; i < Headers.length; i++) {
-        groupHeaders[i] = Headers[i].firstChild.innerHTML;
-    }
-
-    if (target.classList.contains("list-terms__letter-list-item")) {
-        targetIndex = groupHeaders.indexOf(target.innerHTML);
-
-    }
-
-    var scroll_el = $('.list-terms__header-group')[targetIndex];
-    if ($(scroll_el).length != 0) {
-        $('html, body').animate({
-            scrollTop: $(scroll_el).offset().top
-        }, 100);
-    }
-    return false;
-}
-
-listTerms.addEventListener("mouseover", toHeader);
-
 // Вспомогательные функции
 
 function debounce(f, ms) {
-    
+
     var timer = null;
     return function (...args) {
         var onComplete = function () {
@@ -180,50 +109,25 @@ function debounce(f, ms) {
     };
 };
 
-function stringTruncation(str, maxLength) {
-    if (str.length > maxLength) {
-        return str.slice(0, maxLength - 3) + '...';
-    }
-    return str;
-}
-
 // функция запускающая анимацию
 
-function startLoadingAnimation() 
-{
-  
-  var imgObj = $("#loadImg");
-  imgObj.show();
- 
-  var centerY = $(window).scrollTop() + ($(window).height() + imgObj.height())/2;
-  var centerX = $(window).scrollLeft() + ($(window).width() + imgObj.width())/2;
-
-  imgObj.offset({top:centerY,left:centerX});
+function startLoadingAnimation() {
+    var imgObj = document.getElementById('loadImg');
+    imgObj.style.display = "block";
+    imgObj.style.left = (screen.width - imgObj.width) / 2 + "px";
+    imgObj.style.top = (screen.height - imgObj.height) / 3 + "px";
 };
 
 // функция останавливающая анимацию
 
-function stopLoadingAnimation() 
-{
-  $("#loadImg").hide();
-}
-
-// ширина строки
-
-function getTextWidth(text, font) {
-    var canvas = getTextWidth.canvas || (getTextWidth.canvas = document.createElement("canvas"));
-    var context = canvas.getContext("2d");
-    context.font = font;
-    var metrics = context.measureText(text);
-    return metrics.width;
+function stopLoadingAnimation() {
+    var imgObj = document.getElementById('loadImg');
+    imgObj.style.display = "none";
 }
 
 //Поиск и фильтрация из input
 
 function searchTerm() {
-    
-    
-    
     var input, filter, search, item, a;
     input = document.getElementById('search');
     filter = input.value.toUpperCase();
@@ -236,12 +140,9 @@ function searchTerm() {
         if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
             item[i].classList.remove('list-terms__item_wrapper_disable');
             item[i].classList.add('list-terms__item_wrapper');
-
-
         } else {
             item[i].classList.add('list-terms__item_wrapper_disable');
             item[i].classList.remove('list-terms__item_wrapper');
-
         };
 
         item[i].parentNode.childNodes[0].classList.add('list-terms__header-group_disable');
@@ -251,22 +152,18 @@ function searchTerm() {
     var activeTerms = document.getElementsByClassName("list-terms__item_wrapper");
 
     for (var j = 0; j < activeTerms.length; j++) {
-
         activeTerms[j].parentNode.childNodes[0].classList.remove('list-terms__header-group_disable');
         activeTerms[j].parentNode.childNodes[0].classList.add('list-terms__header-group');
     }
 
-  
-    
     stopLoadingAnimation();
-
 };
 
 var search = debounce(searchTerm, 500);
 
 var list = document.getElementsByClassName("list-terms__elements")[0];
 
-function CreateModal(title, description, id, target, favorite, first, last) {
+function CreateModal(title, description, id, target) {
 
     //Модальное окно
 
@@ -300,8 +197,6 @@ function CreateModal(title, description, id, target, favorite, first, last) {
     itemModalMiddle.className = "list-terms__item-modal-middle";
     itemModalMiddle.innerHTML = description;
     itemContent.appendChild(itemModalMiddle);
-
-    
 }
 
 function OpenModal(event) {
@@ -314,7 +209,7 @@ function OpenModal(event) {
 
     if (target.classList.contains("list-terms__item")) {
 
-        CreateModal(target.getAttribute("data-title"), target.getAttribute("data-description"), target.getAttribute("data-id"), target.parentNode, target.getAttribute("data-favorite"), target.getAttribute("data-first_item"), target.getAttribute("data-last_item"))
+        CreateModal(target.getAttribute("data-title"), target.getAttribute("data-description"), target.getAttribute("data-id"), target.parentNode)
     }
 }
 
@@ -330,12 +225,9 @@ function CloseModal(event) {
     }
 };
 
-
-
 var input = document.getElementById("search");
 var inputLoader = debounce(startLoadingAnimation, 400);
 
 input.addEventListener("keyup", inputLoader);
 list.addEventListener("click", CloseModal);
 list.addEventListener("click", OpenModal);
-
